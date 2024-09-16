3453c96d7c74a57d33b558e48d307faff95b49a9
akil <ak1lt4hsin@gmail.com>
2024-09-16 23:02:24 +0600

    Changes made regarding home page and details page

--- resources/views/blog/details.blade.php
+++ resources/views/blog/details.blade.php
@@ -1,2 +1,4 @@
-<h1> {{ $blog->title}} </h1>
-<img src="{{ asset('upload/blog_images/' . $blog->image) }}" alt="{{$blog->title}}">
+<div class="flex w-[200px] p-4 m-5 bg-black">
+  <h1 class="text-xl"> {{ $blog->title}} </h1>
+  <img src="{{ asset('upload/blog_images/' . $blog->image) }}" alt="{{$blog->title}}">
+</div>
